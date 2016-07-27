a8e133394fe8f6f53cb9dc2777e4a42a088357a3
Jason Tam <jzntam@gmail.com>
2016-07-26 23:20:00 -0700

    Postgresify for Heroku Deployment

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20160726201511) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "messages", force: :cascade do |t|
     t.text     "content"
     t.datetime "created_at", null: false
